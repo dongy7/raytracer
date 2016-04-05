4c01ef861e0ee050c4016672fa27a3a986aedc72
dongy7 <dongy7@gmail.com>
2016-04-05 17:45:23 -0400

    refactored ray intersection

--- src/Math/Intersect.hs
+++ src/Math/Intersect.hs
@@ -6,12 +6,13 @@ import Geometry.Object
 import Geometry.Vector
 
 type Solution = (Maybe Scalar, Maybe Scalar)
+type RayScalar = Maybe Scalar
 
--- ((ray origin, ray direction, solutions), surface color))
-type Intersection = ((Vector, Vector, [(Scalar, Surface)]), Colour)
-
+-- ((ray, solutions))
+type Intersection = (Ray, RayScalar, Surface)
 
 -- Solves ax^2 + bx + c = 0
+-- only returns positive solutions
 solveQuad :: Scalar -> Scalar -> Scalar -> Solution
 solveQuad a b c = solns
   where solns | discriminant < 0 = (Nothing, Nothing)
@@ -20,36 +21,34 @@ solveQuad a b c = solns
         sqrtDiscriminant = sqrt discriminant
         rt1 = (-b + sqrtDiscriminant)/ (2*a)
         rt2 = (-b - sqrtDiscriminant)/ (2*a)
-        fstRt = if rt1 > 0
-                   then Just rt1
-                   else Nothing
-        sndRt = if rt2 > 0
-                   then Just rt2
-                   else Nothing
+        fstRt = getPosScalar rt1
+        sndRt = getPosScalar rt2
 
-solveQuadratic :: Scalar -> Scalar -> Scalar -> [Scalar]
-solveQuadratic a b c = solns
-  where solns  | discriminant < 0 = []
-               | otherwise = filter (\x -> x > 0) [(-b + sqrtDiscriminant)/ (2*a), (-b - sqrtDiscriminant)/ (2*a)]
-        discriminant = (b*b) - (4*a*c)
-        sqrtDiscriminant = sqrt discriminant
+-- Returns the smallest positive root
+getMinSoln :: Solution -> RayScalar
+getMinSoln (Nothing, x) = x
+getMinSoln (x, Nothing) = x
+getMinSoln (Just a, Just b) = if a < b
+                                 then Just a
+                                 else Just b
 
+-- Returns the intersection of ray with given surface
 rayIntersect :: Ray -> Surface -> Intersection
-rayIntersect (Ray e d) (Sphere radius center (Material amb dif s p)) =
-  ((e, d, combineSoln (minimum' $ solveQuadratic a b c) (Sphere radius center (Material amb dif s p))) , dif)
+rayIntersect (Ray e d ) (Sphere radius center material) = (Ray e d, minSoln, Sphere radius center material)
   where a = d <*> d
         b = 2 * (d <*> (e <-> center))
         c = (e <-> center) <*> (e <-> center) - radius*radius
-rayIntersect (Ray e d) (Plane n p (Material amb dif s pow)) = ((e, d, solns), dif )
-  where solns  | (d <*> n) == 0 = []
-               | otherwise = filter (\(x, _) -> x > 0) [( numerator/denominator, Plane n p (Material amb dif s pow))]
-        numerator = (p <-> e) <*> n
+        solns = solveQuad a b c
+        minSoln = getMinSoln solns
+rayIntersect (Ray e d) (Plane n p material) = (Ray e d, solns, Plane n p material)
+  where numerator = (p <-> e) <*> n
         denominator = d <*> n
+        quotient = numerator/denominator
+        solns | (d <*> n) == 0 = Nothing
+              | otherwise = getPosScalar quotient
 
-minimum' :: (Ord a) => [a] -> [a]
-minimum' [] = []
-minimum' list = [ minimum list ]
-
-combineSoln :: [a] -> b -> [(a, b)]
-combineSoln [] _ = []
-combineSoln (x:xs) b = [(x, b)] ++ combineSoln xs b
+-- Preserve positive values
+getPosScalar :: Scalar -> Maybe Scalar
+getPosScalar s = if s > 0
+                    then Just s
+                    else Nothing
