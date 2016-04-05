0fa56f740b4dd996b73b57c1f6adba57d6ba518e
dongy7 <dongy7@gmail.com>
2016-04-05 16:13:09 -0400

    add vector module

---
+++ src/Geometry/Vector.hs
@@ -0,0 +1,32 @@
+module Geometry.Vector where
+
+type Scalar = Double
+type Vector = (Scalar, Scalar, Scalar)
+
+(<+>) :: Vector -> Vector -> Vector
+(x1, y1, z1) <+> (x2, y2, z2) = (x1+x2, y1+y2, z1+z2)
+
+(<->) :: Vector -> Vector -> Vector
+(x1, y1, z1) <-> (x2, y2, z2) = (x1-x2, y1-y2, z1-z2)
+
+(<*>) :: Vector -> Vector -> Scalar
+(x1, y1, z1) <*> (x2, y2, z2) = x1*x2 + y1*y2 + z1*z2
+
+mult :: Vector -> Scalar -> Vector
+mult (x, y, z) c = (c*x, c*y, c*z)
+
+vecSum :: Vector -> Scalar
+vecSum (x, y, z) = x + y + z
+
+mag :: Vector -> Scalar
+mag (x, y, z) = sqrt $ x*x + y*y + z*z
+
+normalise :: Vector -> Vector
+normalise (x, y, z) = (x/m, y/m, z/m)
+  where m = mag (x, y, z)
+
+computeBisector :: Vector -> Vector -> Vector
+computeBisector x y = bisector
+  where sumVec = x <+> y
+        sumMag = mag sumVec
+        bisector = mult sumVec (1/sumMag)
