332ad08df8241fcd4a82434d7ddff801569dc773
dongy7 <dongy7@gmail.com>
2016-04-05 22:40:20 -0400

    ad ambient shading

--- src/Math/Shader.hs
+++ src/Math/Shader.hs
@@ -27,6 +27,27 @@ phongShade (Plane norm point (Material amb dif surf pow)) vec = scaleColour surf
         dot = normN <*> h
         k = intensity * ((max' (0, dot)) ** pow)
 
+-- compute the diffuse shading for a given point on surface
+diffuseShade :: Surface -> Vector -> Colour
+diffuseShade (Sphere rad cen (Material am dif sur pow)) vec = scaleColour dif k
+  where normN = normalise $ computeSurfNorm (Sphere rad cen (Material am dif sur pow)) vec
+        normL = normalise lVec
+        lVec = lightSource <-> vec
+        dot = normL <*> normN
+        k = intensity * (max' (0, dot))
+diffuseShade (Plane norm point (Material am dif sur pow)) vec = scaleColour dif k
+  where normN = normalise $ computeSurfNorm (Plane norm point (Material am dif sur pow)) vec
+        normL = normalise l
+        l = lightSource <-> vec
+        dot = normL <*> normN
+        k = intensity * (max' (0, dot))
+
+-- compute the ambient shading for a given point on surface
+computeAmbShading :: Surface -> Colour
+computeAmbShading  (Sphere _ _ (Material amb _ _ _)) = scaleColour amb intensity
+computeAmbShading  (Plane _ _ (Material amb _ _ _)) = scaleColour amb intensity
+
+-- compute the normal for a point on a surface
 computeSurfNorm :: Surface -> Vector -> Vector
 computeSurfNorm (Sphere _ center _) vec = vec <-> center
 computeSurfNorm (Plane normal _ _) _ = normal
