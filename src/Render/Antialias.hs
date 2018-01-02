fe4a99f46feb2562a367d2f4b89bedb530d7c9d5
Dong Yeop Lee <dongy7@gmail.com>
2018-01-02 15:43:38 -0500

    cleaned up antialias

--- src/Render/Antialias.hs
+++ src/Render/Antialias.hs
@@ -7,29 +7,17 @@ import Render.Raytracer
 import System.Random
 
 boxFilter :: Scene -> Scalar -> Scalar -> Color
-boxFilter scene i j = avgColor
+boxFilter scene i j = getAverageColor $ map (computePixelPairColor scene) randomPixels
   where randomPixels = genRandomPixels i j
-        shadings = map (computePixelPairColor scene) randomPixels
-        avgColor = getAverageColor shadings
 
 -- get average color from color list
 getAverageColor :: [Color] -> Color
-getAverageColor colors = avg
+getAverageColor colors = scaleColor colorSum (1/len)
   where colorSum = foldl addColor (0, 0, 0) colors
         len = fromIntegral $ length colors
-        reciprocal = 1/len
-        avg = scaleColor colorSum reciprocal
-
 
 -- generates random sample of pixels within the boundaries of a given pixel
 genRandomPixels :: Scalar -> Scalar -> [(Scalar, Scalar)]
-genRandomPixels i j = randomCoords
+genRandomPixels i j = map (\(x,y) -> (x+i, y+j)) $ zip randomXs randomYs
   where randomXs = take sampleSize $ randomRs (-0.5, 0.5) (mkStdGen seedX) :: [Double]
         randomYs = take sampleSize $ randomRs (-0.5, 0.5) (mkStdGen seedY) :: [Double]
-        combined = zip randomXs randomYs
-        ij = replicate sampleSize (i, j)
-        randomCoords = zipWith addTuple combined ij
-
-
-addTuple :: (Scalar, Scalar) -> (Scalar, Scalar) -> (Scalar, Scalar)
-addTuple (x, y) (x', y') = (x+x', y+y')
